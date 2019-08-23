9038658dd9fad48d77eccc10d414d7307f196e97
chrisjsimpson <chris@karmacomputing.co.uk>
2019-08-23 01:54:17 +0100

    added very cude webpage+form

--- bank_viewer/main.py
+++ bank_viewer/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, render_template, url_for, redirect
 from hsbc_csv_parse import (get_records, calculate, search, 
                     total_income, total_payments) 
 
@@ -7,9 +7,19 @@ from hsbc_csv_parse import (get_records, calculate, search,
 def create_app(test_config=None):
   app = Flask(__name__)
 
+  @app.route("/")
+  def home():
+    return render_template('layout.html')
+
+  @app.route("/", methods=["POST"])
+  def route_transactions_request():
+    year = request.form['year']
+    month = request.form['month']
+    return redirect(url_for('transactions_by_year_month', year=year, month=month))
+
   @app.route("/transactions/<year>/<month>", methods=["GET"])
   def transactions_by_year_month(year, month):
     
-    return str(calculate(year=year, month=month))
+    return render_template('layout.html', body=calculate(year=year, month=month))
 
   return app
