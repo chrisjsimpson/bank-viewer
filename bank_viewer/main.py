7353d9ddfb444aff8aa16fde27e0f247b5571635
chrisjsimpson <chris@karmacomputing.co.uk>
2019-08-25 00:29:37 +0100

    added subprocess to kick off scraping when /scrape/code is called

--- bank_viewer/main.py
+++ bank_viewer/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, url_for, redirect
 from hsbc_csv_parse import (get_records, calculate, search, 
-                    total_income, total_payments) 
+                    total_income, total_payments)
+from subprocess import run
 
 
 
@@ -19,7 +20,15 @@ def create_app(test_config=None):
 
   @app.route("/transactions/<year>/<month>", methods=["GET"])
   def transactions_by_year_month(year, month):
-    
     return render_template('layout.html', body=calculate(year=year, month=month))
+  
+  @app.route("/scrape/<code>", methods=["GET"])
+  def scrape(code):
+    code = int(code)
+    cmd = "printf '{}\n' | docker exec -i hsbc-scrape python3 main.py".format(code)
+    print("Running subprocess: {}".format(cmd))
+    run(cmd, shell=True) # Run as subprocess NOTE may need to edit sudoers (visudo)
+    return "Done"
+    
 
   return app
